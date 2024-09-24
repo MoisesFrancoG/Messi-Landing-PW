505a2dba09c80b02286217929f116207adf80cab
MoisesFranco <moisesfrancogtz@gmail.com>
2024-09-23 21:29:01 -0600

    add: inicializacion de componentes del body

---
+++ src/app/body/body/trophies/trophies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trophies',
+  templateUrl: './trophies.component.html',
+  styleUrl: './trophies.component.css'
+})
+export class TrophiesComponent {
+
+}
